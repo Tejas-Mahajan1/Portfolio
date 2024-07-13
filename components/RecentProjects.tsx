4bedf315083ac0c40667cf1bb8ecb69e3f68aa3f
Tejas Mahajan <114793178+Tejas-Mahajan1@users.noreply.github.com>
2024-07-13 10:31:45 +0530

    Few Image tag changes

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -64,7 +64,13 @@ const RecentProjects = () => {
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      <Image
+                        src={icon}
+                        alt="icon5"
+                        className="p-2"
+                        width={32}
+                        height={32}
+                      />
                     </div>
                   ))}
                 </div>
