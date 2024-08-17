cf9c9f8aadd259efcdcb735d8fc9077dc00badcb
Tejas Mahajan <114793178+Tejas-Mahajan1@users.noreply.github.com>
2024-08-17 20:54:09 +0530

    Updated Projects

--- data/index.ts
+++ data/index.ts
@@ -94,14 +94,14 @@ export const projects = [
     iconLists: ["/re.svg", "/tail.svg", "/js.svg", "git.svg"],
     link: "https://tejas-mahajan-imdb.netlify.app/",
   },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://cloned-apple-iphone-15-launch.vercel.app/",
-  },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "https://cloned-apple-iphone-15-launch.vercel.app/",
+  // },
   // {
   //   id: 4,
   //   title: "AI Image SaaS - Canva Application",
